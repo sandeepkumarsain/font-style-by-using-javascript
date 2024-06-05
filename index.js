8bd099b13f6a21bb979b7233d75dc5788e5ee6e1
sandeepkumarsain <sainsandeep032@gmail.com>
2024-06-05 12:29:03 +0530

    font style

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function blod()
+{
+   var a=document.getElementById('text')
+   var b=document.getElementById('btn1')
+   a.style.fontWeight="bold"
+
+}
+function italic()
+{
+   var a=document.getElementById('text')
+   var b=document.getElementById('btn2')
+   a.style.fontStyle="italic"
+
+
+}
+function upper()
+{
+   var a=document.getElementById('text')
+   var b=document.getElementById('btn3')
+   a.style.textTransform="uppercase"
+
+
+}
+
+function lower()
+{
+   var a=document.getElementById('text')
+   var b=document.getElementById('btn2')
+   a.style.textTransform="lowercase"
+
+
+}
+   
